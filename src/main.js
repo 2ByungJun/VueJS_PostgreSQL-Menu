3106af0d2516e00002e5bb670f9dd4c4d84795ff
2ByungJun <ieed0205@naver.com>
2021-01-12 15:57:45 +0900

    미미한 수정

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import VueTreeList from 'vue-tree-list'
 
 Vue.use(BootstrapVue)
